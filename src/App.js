5249686075b83e1739bb0b1bcbf29f8fbc007ccc
RafaS92 <manrvaldez..92@gmail.com>
2020-11-08 18:29:53 -0600

    ready beta

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Provider, Route } from '@triframe/designer'
 import { Vote} from './views/Vote'
 import { Voter} from './views/Voter'
-import { Login } from './views/Login';
 import "./App.css"
 import {DumLogin} from "./views/DumLogin"
 import {DumWelcome} from "./views/DumWelcome"
@@ -15,8 +14,8 @@ export default () => (
     <Provider url={process.env.REACT_APP_BACKEND_URL}>
         <Route exact path="/vote" component={Vote} />
         <Route exact path="/voter" component={Voter} />
-        <Route exact path="/" component={Login} />
-        <Route exact path="/welcome" component={Welcome} />
+        <Route exact path="/" component={DumLogin} />
+        <Route exact path="/welcome" component={DumWelcome} />
         <Route exact path="/novote" component={NoVote} />
         <Route exact path="/thanksvote" component={ThanksVote} />
 
--- src/views/DumLogin.js
+++ src/views/DumLogin.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import {tether, Container, Heading, Button, TextInput, Text} from '@triframe/designer'
+import {tether, Container, Heading, Button, TextInput,PasswordInput, Text,HelperText} from '@triframe/designer'
 import { Paragraph } from '@triframe/designer/dist/paper'
 
 
 
 export const DumLogin= tether(function*({Api, redirect}){
 
-    const {Voter} = Api
+    const { Voter } = Api;
 
     const form = yield {
+        firstname: '',
+        lastname:  '',
+        votersNumber: '',
+        password: '',
+        errorMessage: null,
         username: ''
     }
 
@@ -16,30 +21,50 @@ export const DumLogin= tether(function*({Api, redirect}){
 
     
 
-    <div className="login-wrapper ">
+    <div className="login-wrapper">
 
        
        
           <div className="form-container">
           <h2 className="login-form-head">Login</h2>
 
-          <TextInput
-                label="Username"
-                value={form.username}
-                onChange={value => form.username = value}
-          />
-          <TextInput // add this dont know if we have to Rafa
+          <TextInput 
+                label="First Name"
+                value={FormData.firstname}
+                onChange={value => form.firstname = value}
+            />
+
+            <TextInput 
+                label="Last Name"
+                value={FormData.lastname}
+                onChange={value => form.lastname = value}
+            />
+
+            <TextInput 
+                label="Voter Number"
+                value={FormData.votersNumber}
+                onChange={value => form.votersNumber = value}
+            />
+
+            <PasswordInput 
                 label="Password"
-                value={form.password} 
-                onChange={value => form.username = value}
-                
-          />
-          <Button onPress={async () => {
-              await Voter.login(form.username)
-              redirect('/welcome')
-              }}> 
-            Login
-          </Button>
+                value={FormData.password}
+                onChange={value => form.password = value}
+            />
+
+            <Button onPress={async () => {
+                try{
+                    await Voter.login(form.firstname, form.password)
+                    redirect('/welcome')
+                }catch(error){
+                    form.errorMessage = error.message;
+                }
+            }}>
+                Login 
+            </Button>
+            <HelperText type="error" visible={form.errorMessage !== null}>
+                {form.errorMessage}
+            </HelperText>
 
           </div>
           </div>
